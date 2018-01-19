a9c4e66b10179c29ea8176ee49c163599bdaa2b9
Ousmanendiaye352 <ousmanendiaye352@gmail.com>
2018-01-19 20:47:26 +0000

    et Ousmane encore

--- index.php
+++ index.php
@@ -48,6 +48,45 @@
           $(this).tab('show');
         });
       </script>
+
+      <!-- Encore Ousmane ndiaye Encore Ousmane ndiaye Encore Ousmane ndiaye Encore Ousmane ndiaye Encore Ousmane ndiaye Encore Ousmane ndiaye -->
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs|pills" id="navId">
+        <li class="nav-item">
+          <a href="#tab1Id" class="nav-link active">Active</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#tab2Id">Action</a>
+            <a class="dropdown-item" href="#tab3Id">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#tab4Id">Action</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a href="#tab5Id" class="nav-link">Another link</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <div class="tab-pane fade in active" id="tab1Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab2Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab3Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab4Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab5Id" role="tabpanel"></div>
+      </div>
+      
+      <script>
+        $('#navId a').click(e => {
+          e.preventDefault();
+          $(this).tab('show');
+        });
+      </script>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
